b52d6f9c1583f76452e49d70dddc47d1b48cdd57
junyi <1029842003@qq.com>
2019-12-20 17:59:14 +0800

    购物车，详情页，注册

--- ZOLstore/src/js/register.js
+++ ZOLstore/src/js/register.js
@@ -1,92 +1,108 @@
 import { log, $, cookietool, ajax } from './tool.js';
-class FormValidator{
-    constructor(){
+class FormValidator {
+    constructor() {
         this.oInp = $('.register-form .text');
         this.oWrong = $('.register-form .wrong-tips');
         this.oRight = $('.register-form .right-tips');
+        this.oSub = $('#J_register_phone_submit');
         this.phoneFlag = false;
         this.passwordFlag = false;
         this.rePassFlag = false;
         this.emailFlag = false;
     }
-    init(){
+    init() {
         this.register();
+        this.subRegister();
     }
     //表单验证函数
-    register(){
+    register() {
         //手机号判断
-        this.oInp[0].onblur = ()=>{
-            let phone_num =this.oInp[0].value;//手机号码的值
-            if(phone_num != ''){//判断输入是否为空
-                if(phone_num.length==11){//判断手机号是否小于11位(最大长度已经限制11位数)
-                    if(/^1[3578]\d{9}$/.test(phone_num)){//正则判断
+        this.oInp[0].onblur = () => {
+            let phone_num = this.oInp[0].value;//手机号码的值
+            if (phone_num != '') {//判断输入是否为空
+                if (phone_num.length == 11) {//判断手机号是否小于11位(最大长度已经限制11位数)
+                    if (/^1[3578]\d{9}$/.test(phone_num)) {//正则判断
                         this.oWrong[0].style.display = 'none';
                         this.oRight[0].style.display = 'block';
                         this.phoneFlag = true;
-                    }else{
+                    } else {
                         this.oWrong[0].style.display = 'block';
-                        this.oWrong[0].innerHTML = '手机号格式有误' ;
+                        this.oWrong[0].innerHTML = '手机号格式有误';
                     }
-                }else{
+                } else {
                     this.oWrong[0].style.display = 'block';
                     this.oWrong[0].innerHTML = '手机号不能少于11位';
                 }
-            }else{
+            } else {
                 this.oWrong[0].style.display = 'block';
                 this.oWrong[0].innerHTML = '手机号不能为空';
             }
         }
         //密码判断
-        this.oInp[1].onblur = ()=>{
+        this.oInp[1].onblur = () => {
             let password = this.oInp[1].value;
-            if(password.length>=6){
-                if(/^\w+$/.test(password)){
-                    if(/[a-zA-Z]+/g.test(password)&&/[0-9]+/g.test(password)){
+            if (password.length >= 6) {
+                if (/^\w+$/.test(password)) {
+                    if (/[a-zA-Z]+/g.test(password) && /[0-9]+/g.test(password)) {
                         this.oWrong[1].style.display = 'none';
                         this.oRight[1].style.display = 'block';
                         this.passwordFlag = true;
-                    }else{
+                    } else {
                         this.oWrong[1].style.display = 'block';
                         this.oWrong[1].innerHTML = '密码必须是数字字母组合';
                     }
-                }else{
+                } else {
                     this.oWrong[1].style.display = 'block';
                     this.oWrong[1].innerHTML = '不可用特殊字符';
                 }
-            }else{
+            } else {
                 this.oWrong[1].style.display = 'block';
                 this.oWrong[1].innerHTML = '请输入6-16位数密码';
             }
         }
         //确认密码
-        this.oInp[2].onblur = ()=>{
-            if(this.oInp[2].value==this.oInp[1].value){
+        this.oInp[2].onblur = () => {
+            if (this.oInp[2].value == this.oInp[1].value) {
                 this.oWrong[2].style.display = 'none';
                 this.oRight[2].style.display = 'block';
                 this.rePassFlag = true;
-            }else{
+            } else {
                 this.oWrong[2].style.display = 'block';
                 this.oWrong[2].innerHTML = '密码不一致';
             }
         }
         //邮箱验证
-        this.oInp[3].onblur = ()=>{
+        this.oInp[3].onblur = () => {
             let email = this.oInp[3].value;
-            if(email!=''){
-                if(/^(\w+[\+\-\_\.]*\w+)\@(\w+[\+\-\_\.]*\w+)\.(\w+[\+\-\_\.]*\w+)$/.test(email)){
+            if (email != '') {
+                if (/^(\w+[\+\-\_\.]*\w+)\@(\w+[\+\-\_\.]*\w+)\.(\w+[\+\-\_\.]*\w+)$/.test(email)) {
                     this.oWrong[3].style.display = 'none';
                     this.oRight[3].style.display = 'block';
                     this.emailFlag = true;
-                }else{
+                } else {
                     this.oWrong[3].style.display = 'block';
                     this.oWrong[3].innerHTML = '邮箱格式有误';
                 }
-            }else{
+            } else {
                 this.oWrong[3].style.display = 'block';
                 this.oWrong[3].innerHTML = '邮箱不能为空';
             }
         }
     }
+    //提交
+    subRegister() {
+        this.oSub.onclick = () => {
+            if (this.phoneFlag && this.passwordFlag && this.rePassFlag && this.emailFlag) {
+                setTimeout(function(){
+                    alert('注册成功');
+                    $('#J_register_phone_loading').style.display='none';
+                },1000);
+                $('#J_register_phone_loading').style.display='block';
+                
+            }
+        }
+    }
+
 
 }
 export { FormValidator }
